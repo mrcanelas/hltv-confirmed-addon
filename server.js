a2a5083d28a31b324f8d3c97edf8437ff53dcfbb
mrcanelas <sialsan@hotmail.com>
2022-11-05 20:51:28 -0300

    add update catalog function

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
-const addonInterface = require("./addon")
-serveHTTP(addonInterface, { port: process.env.PORT || 53832 })
+const { serveHTTP, publishToCentral } = require("stremio-addon-sdk");
+const schedule = require("node-schedule");
+const addonInterface = require("./addon");
+const axios = require("axios");
+
+serveHTTP(addonInterface, { port: process.env.PORT || 53832 }).then((res) => {
+  schedule.scheduleJob("* */15 * * * ", function () {
+    axios
+      .get(
+        "https://94c8cb9f702d-hltv-confirmed-addon.baby-beamup.club/catalog/tv/top.json?break"
+      )
+      .then((resp) => console.log("Catalog Updated"));
+  });
+});
