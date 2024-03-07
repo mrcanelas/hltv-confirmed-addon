aea5cdd926e54bf41fcef68091984ef24ae3f6c2
mrcanelas <sialsan@hotmail.com>
2024-03-07 15:52:54 -0300

    update to 0.0.3 version

--- addon.js
+++ addon.js
@@ -1,60 +1,29 @@
-const { addonBuilder } = require("stremio-addon-sdk");
-const { HLTV } = require("hltv");
-const { getStreams } = require("./getStreams");
-const { getMeta } = require("./getMeta");
-const { cacheWrapMeta } = require("./cache");
+const { addonBuilder } = require("stremio-addon-sdk")
+const manifest = require("./lib/getManifest")
+const { cacheWrapMeta } = require("./lib/getCache")
+const { getCatalog } = require("./lib/getCatalog")
+const { getMeta } = require("./lib/getMeta")
+const { getStreams } = require("./lib/getStreams")
+const builder = new addonBuilder(manifest)
 
-const manifest = {
-  id: "community.HLTV",
-  version: "0.0.2",
-  catalogs: [
-    {
-      type: "tv",
-      id: "top",
-    },
-  ],
-  resources: ["catalog", "stream", "meta"],
-  types: ["tv"],
-  name: "HLTV Confirmed",
-  description:
-    "Watch the biggest Counter Strike: Global Offensive championships in real time.",
-  background:
-    "https://res.cloudinary.com/dnailepwh/image/upload/v1667141869/hltv/ffc38d9bce22d7a9eaa07b890e3af1a2_feobw4.jpg",
-  logo: "https://res.cloudinary.com/dnailepwh/image/upload/v1667142999/hltv/D9lBEYYW4AIKgiP_c8ivib.png",
-};
-const builder = new addonBuilder(manifest);
-
-builder.defineCatalogHandler(async () => {
-  return new Promise(async (resolve, reject) => {
-    HLTV.getMatches().then(async (matches) => {
-      const inLive = matches.filter((val) => val.live === true);
-      const metas = await Promise.all(
-        inLive.map(async (match) => {
-          const meta = await cacheWrapMeta(match.id, async () => {
-            return await getMeta(match.id);
-          });
-          return meta;
-        })
-      );
-      resolve({ metas });
-    });
-  });
-});
+builder.defineCatalogHandler(async ({ type, id, extra }) => {
+	return await getCatalog()
+})
 
 builder.defineMetaHandler(async ({ id }) => {
-  const matchId = id.split(":")[1];
-  const meta = await cacheWrapMeta(matchId, async () => {
-    return await getMeta(matchId);
-  });
-  return Promise.resolve({ meta });
-});
+	const matchId = id.split(":")[1];
+	const meta = await cacheWrapMeta(matchId, async () => {
+		return await getMeta(matchId);
+	});
+	return Promise.resolve({ meta });
+})
 
 builder.defineStreamHandler(({ id }) => {
-  return new Promise(async (resolve, reject) => {
-    getStreams(id)
-      .then((streams) => resolve(streams))
-      .catch((err) => reject(err));
-  });
-});
+	return new Promise(async (resolve, reject) => {
+		getStreams(id)
+		  .then((streams) => resolve(streams))
+		  .catch((err) => reject(err));
+	  });
+})
 
-module.exports = builder.getInterface();
+module.exports = builder.getInterface()
--- server.js
+++ server.js
@@ -1,14 +1,3 @@
-const { serveHTTP, publishToCentral } = require("stremio-addon-sdk");
-const schedule = require("node-schedule");
-const addonInterface = require("./addon");
-const axios = require("axios");
-
-serveHTTP(addonInterface, { port: process.env.PORT || 53832 }).then((res) => {
-  schedule.scheduleJob("* */15 * * * ", function () {
-    axios
-      .get(
-        "https://94c8cb9f702d-hltv-confirmed-addon.baby-beamup.club/catalog/tv/top.json?break"
-      )
-      .then((resp) => console.log("Catalog Updated"));
-  });
-});
+const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
+const addonInterface = require("./addon")
+serveHTTP(addonInterface, { port: process.env.PORT || 52482 })
